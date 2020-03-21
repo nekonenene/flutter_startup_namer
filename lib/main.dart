4e1e4386c356db6a0b17251fe420fc116a0686b8
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-03-22 04:22:02 +0900

    ListView が表示されるように

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -32,7 +25,43 @@ class RandomWordsState extends State<RandomWords> {
   // State において build メソッドの実装は必須
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  // アンダースコアを付けると private になる
+  final _suggestions = <WordPair>[]; // リストに表示される単語一覧
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  // 単語と区切り線が交互に出てくる ListView を作成する
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // ListView は itemBuilder プロパティを持つ
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider(); // 奇数回目で Divider（仕切り）を設置（ i は 0 始まりなので実質偶数番目に区切り線は置かれる）
+
+          final index = i ~/ 2; // ~/ は切り捨て。単語リストの他に区切り線が表示されるので 2 で割っている
+          if (index >= _suggestions.length) {
+            // リストをすべて読み込むとさらに 10 追加される
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          // 偶数回目では Divider でなく ListTile を設置する
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  // WordPair をもとに ListTile を作成
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
