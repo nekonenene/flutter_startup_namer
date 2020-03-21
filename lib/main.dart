37f761c866312e01498c67a30c8544b979ed3517
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-03-22 06:56:35 +0900

    AppBarにリストアイコン表示

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,15 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
+  }
+
+  void _pushSaved() {
   }
 
   // アンダースコアを付けると private になる
