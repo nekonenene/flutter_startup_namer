fdd83d2b7aaa0ab2b9169f573888067036967c2b
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-03-22 03:51:07 +0900

    State の利用

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,10 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// StatelessWidget はすべての値が final （変更不可）
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -15,9 +14,25 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+// StatefulWidget は StatelessWidget と異なり値の変更が可能。ここでは State の作成だけおこなう
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+// State の実装。ロジックはここに分離される
+class RandomWordsState extends State<RandomWords> {
+  // State において build メソッドの実装は必須
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
