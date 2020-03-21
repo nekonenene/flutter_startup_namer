46cc6218bbc02f4f506b4ce6dad74aeed6bd4d57
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2020-03-22 07:16:41 +0900

    テーマカラーを白に

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: RandomWords(),
     );
   }
